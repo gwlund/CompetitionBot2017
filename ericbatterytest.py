ea92cda133449ef63cac801e2f61a43e7de0d246
Eric Newcomer <Eric Newcomer>
2017-01-18 18:04:06 -0800

    Added rough draft battery test

---
+++ ericbatterytest.py
@@ -0,0 +1,29 @@
+
+import wpilib
+import seamonsters.fix2017
+
+class BatteryTest(wpilib.IterativeRobot):
+
+    def robotInit(self):
+        bl = wpilib.CANTalon(1)
+        fl = wpilib.CANTalon(3)
+        fr = wpilib.CANTalon(0)
+        br = wpilib.CANTalon(2)
+        self.talons = [bl,fl,fr,br]
+
+    def autonomousInit(self):
+        self.count = 0
+
+    def autonomousPeriodic(self):
+        self.count = self.count + 1
+        if self.count < 250:
+            self.allMotors(1)
+        else:
+            self.allMotors(0)
+
+    def allMotors(self,speed):
+        for talon in self.talons:
+            talon.set(speed)
+
+if __name__ == "__main__":
+    wpilib.run(BatteryTest)
